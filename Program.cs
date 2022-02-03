193df6814f120e5be10b922205100b645c03664b
Daniel Cronqvist <dancro99@gmail.com>
2022-02-03 19:00:42 +0100

    Everything you need

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using static Raylib_cs.Raylib;
+using static Raylib_cs.Color;
+using ImGuiNET;
+
+namespace raylib_imgui_dotnet_template;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Initialization
+        //--------------------------------------------------------------------------------------
+        const int screenWidth = 1280;
+        const int screenHeight = 720;
+
+        InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+        ImGuiController igc = new ImGuiController();
+        igc.Load(screenWidth, screenHeight);
+
+        SetTargetFPS(60);
+        //--------------------------------------------------------------------------------------
+
+        // Main game loop
+        while (!WindowShouldClose())    // Detect window close button or ESC key
+        {
+            // Update
+            //----------------------------------------------------------------------------------
+            // TODO: Update your variables here
+            //----------------------------------------------------------------------------------
+            igc.Update(GetFrameTime());
+
+            ImGui.ShowDemoWindow();
+
+            // Draw
+            //----------------------------------------------------------------------------------
+            BeginDrawing();
+            ClearBackground(RAYWHITE);
+
+            DrawText("Congrats! You created your first window!", 350, 320, 20, MAROON);
+
+            igc.Draw();
+
+            EndDrawing();
+            //----------------------------------------------------------------------------------
+        }
+
+        // De-Initialization
+        //--------------------------------------------------------------------------------------
+        CloseWindow();        // Close window and OpenGL context
+                              //--------------------------------------------------------------------------------------
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # raylib-imgui-dotnet-template
+
+🎨 template repo for getting started with raylib together with imgui using dotnet!
+
+## Getting started
+
+1. Click the green `Use this template` up to the right and choose a name for the new repository.
+2. Clone down your new repository.
+3. `dotnet run` inside the repo gets you going!
